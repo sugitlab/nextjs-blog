be0f44c5ed3872faef9a12963eba0b4e2a6fb5d4
sugitlab <sgmt.snj@gmail.com>
2021-02-16 23:37:54 +0900

    change: change index.js to explain the site clearly

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>We are the team designing the exciting future.</p>
+        <p>Hi, here is the first nextjs blog designed by sugit </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
