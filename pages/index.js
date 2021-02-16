f1cd4d3aa3f6bbe20c8a17559d32cc252b2721e1
sugitlab <sgmt.snj@gmail.com>
2021-02-16 22:31:07 +0900

    status 390 points

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
@@ -27,9 +28,7 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-                {title}
-              <br />
-                ID: {id}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
                 Date: {date}
             </li>
